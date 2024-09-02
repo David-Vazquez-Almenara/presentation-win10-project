9d411638da283c21f63bc92421be373f0735b9c4
David-Vazquez-Almenara <david.vaz.almenara@gmail.com>
2024-09-02 14:07:18 +0200

    implementacion de iconos

--- src/Component/DesktopBackground.js
+++ src/Component/DesktopBackground.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import '../CSS/DesktopBackground.css';
+import appData from '../Data/appsData.json'; // Importa los datos de las aplicaciones
+import '../CSS/DesktopBackground.css'; // Importa el CSS correcto
 
-const DesktopBackground = () => {
+function DesktopBackground() {
   return (
-    <div id='Desktop-Background'>
-      {/* Aquí puedes agregar íconos de aplicaciones si es necesario */}
+    <div id="Desktop-Background">
+
+
+      {/* Renderiza los íconos de las aplicaciones */}
+
+      <div className="icon-grid"> 
+        {appData.applications.map((app) => (
+          <div key={app.id} className="app-icon"> {/* Crea una key con la ID usando el JSON */}
+            <img
+            // Usa la propiedad 'app.X' del JSON
+              src={app.icon} 
+              alt={app.name + " icon"}
+              className="icon-image"
+            />
+            <p>{app.name}</p>
+          </div>
+        ))}
+      </div>
+
+
     </div>
   );
-};
+}
 
 export default DesktopBackground;
--- src/Component/Window.js
+++ src/Component/Window.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import Draggable from 'react-draggable';
 import '../CSS/Window.css';
-import appData from '../Data/appsData.json'; // Asegúrate de que la ruta sea correcta
-import ApplicationsMap from './ApplicationsMap'; // Importa el mapeo de componentes
+import appData from '../Data/appsData.json';
+import ApplicationsMap from './ApplicationsMap';
 
 const Window = () => {
   return (
     <>
       {appData.applications.map(app => {
-        const ComponentToRender = ApplicationsMap[app.component]; // Obtén el componente desde el mapeo
+        const ComponentToRender = ApplicationsMap[app.component]; // Obtén el componente desde el ApplicationsMap
 
         return (
           <Draggable
             key={app.id}
-            bounds="#Desktop-Background" // Limita el área de arrastre al contenedor especificado por el ID
+            bounds="#Desktop-Background" // Limita el área de arrastre
           >
             <div className='window'>
               <div className='window-header'>
